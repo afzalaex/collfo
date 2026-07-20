39fedbb3eebbb591a06842531e9d771817afd1a2
Afzal <afzalaex@gmail.com>
2026-07-20 17:55:03 +0530

    Center navbar title

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -3,24 +3,7 @@ import Link from "next/link";
 export function SiteNav() {
   return (
     <nav aria-label="Main" className="site-navbar">
-      <div className="site-navbar__content">
-        <a
-          href="https://aex.design"
-          className="site-navbar__logo"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span style={{ display: "contents" }}>
-            <img
-              src="/assets/logo.svg"
-              alt="Aex Designs"
-              width={80}
-              height={55}
-              decoding="async"
-              loading="eager"
-            />
-          </span>
-        </a>
+      <div className="site-navbar__content" style={{ justifyContent: "center" }}>
         <Link href="/" className="site-navbar__title">
           collfo
         </Link>
