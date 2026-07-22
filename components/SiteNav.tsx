7db22bdce1cd37a77de608a0d87aec13c36312d6
Afzal <afzalaex@gmail.com>
2026-07-22 18:59:25 +0530

    Update navbar logo to use custom SVG hover animation

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -4,10 +4,14 @@ export function SiteNav() {
   return (
     <nav aria-label="Main" className="site-navbar">
       <div className="site-navbar__content" style={{ justifyContent: "center" }}>
-        <Link href="/" className="site-navbar__title group">
-          <span className="logo-letter">c</span>
-          <span className="nav-logo-middle">ollf</span>
-          <span className="logo-letter">o</span>
+        <Link href="/" className="site-navbar__logo-group group">
+          <div className="logo-monogram">
+            <img src="/assets/monogram-left.svg" alt="Collfo left" className="logo-part-left" />
+            <img src="/assets/monogram-right.svg" alt="Collfo right" className="logo-part-right" />
+          </div>
+          <div className="logo-full-wrapper">
+            <img src="/assets/full-logo.svg" alt="Collfo" className="logo-full-img" />
+          </div>
         </Link>
       </div>
     </nav>
